93841e394a967466750b2234c744babc6d727f1b
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-05 02:13:45 +0900

    Update 자릿수 더하기.js

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,3 +1,16 @@
 function solution(n) { 
     return n.toString().split('').reduce((a,c) => a+ Number(c), 0);
 }
+
+/*
+
+function solution2(n) { 
+    var answer = 0;
+    while(n>0){
+        answer += (n%10);
+        n = Math.floor(n/10);
+    }
+    return answer;
+}
+
+*/
