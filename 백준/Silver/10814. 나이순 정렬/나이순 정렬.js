71fd3d706e67d6f6026275aefbcf124b2b4a8f44
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-28 16:47:43 +0900

    [Silver V] Title: 나이순 정렬, Time: 1208 ms, Memory: 30728 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const len = input.shift();
+
+input.sort((a, b) => a.split(" ")[0] - b.split(" ")[0]);
+
+console.log(input.join("\n"));
