8d6c65090f05f0700363b726f41ea89cbe1aed89
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-10-08 00:18:57 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,6 +1,4 @@
 function solution(n)
 {
-    var answer = [];
-    answer = (n.toString().split(''));   
-    return answer.map(v => Number(v)).reduce((a,c)=>a+c);
+    return (n+'').split('').reduce((a,c) => a+parseInt(c), 0)
 }
