ea3c322e24c150b69cd74683e75207f58644580b
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-10-15 00:43:03 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,12 +1,6 @@
 function solution(phone_number) {
     let answer = '';
-    let str = '';
-    let length = phone_number.length-4;
+    answer = '*'.repeat(phone_number.length-4) + phone_number.slice(-4);
     
-    for(let i=0; i<length; i++){
-        str += '*';
-    }    
-    
-    answer = phone_number.substr(-4);
-    return str+answer;
+    return answer;
 }
