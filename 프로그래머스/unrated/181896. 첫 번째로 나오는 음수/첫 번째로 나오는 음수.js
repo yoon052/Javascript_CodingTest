1298f6d7d5f0c16dbe3140bb6bb81b036cbb3dcd
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-11-24 01:07:12 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,8 +1,3 @@
 function solution(num_list) {
-    var answer = 0;
-    if(num_list.filter(v => v<0)){
-        return num_list.indexOf(num_list.find(v => v<0));
-    } else {
-        return -1;
-    }
+    return num_list.findIndex(v => v<0);
 }
