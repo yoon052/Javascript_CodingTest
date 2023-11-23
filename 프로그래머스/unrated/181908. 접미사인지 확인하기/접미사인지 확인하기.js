d1c7f05231d6de00e0c6bb4fa45baa3fabbcf39f
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-11-24 05:16:11 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,3 @@
 function solution(my_string, is_suffix) {
-    return my_string.indexOf(is_suffix) + is_suffix.length === my_string.length ? 1 : 0;
+    return my_string.endsWith(is_suffix) ? 1 : 0;
 }
