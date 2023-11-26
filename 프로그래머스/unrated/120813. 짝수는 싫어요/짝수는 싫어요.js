8203cbce934207683cfce5e231f17647eefa8a80
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-11-26 09:23:08 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,9 +1,7 @@
 function solution(n) {
     var answer = [];
-    for(let i=1; i<=n; i++){
-        if(i%2 === 1){
-            answer.push(i);
-        }        
+    for(let i=1; i<=n; i+=2){
+        answer.push(i);
     }
     return answer;
 }
